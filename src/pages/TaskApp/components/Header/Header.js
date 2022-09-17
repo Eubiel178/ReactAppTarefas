8a48c3c3430325193d3a8f30343f0948b16ac402
Eubiel178 <98427107+Eubiel178@users.noreply.github.com>
2022-09-17 12:52:18 -0300

    Removi a barra de navegacao

--- src/pages/TaskApp/components/Header/Header.js
+++ src/pages/TaskApp/components/Header/Header.js
@@ -1,11 +1,9 @@
 import { HeaderContents } from "./Styles";
-import NavBar from "../../../../components/NavBar/NavBar";
 
 const Header = () => {
   return (
     <header>
       <HeaderContents>
-        <NavBar />
         <h1>ADICIONAR TAREFAS</h1>
       </HeaderContents>
     </header>
