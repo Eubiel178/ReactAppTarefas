c823b9a27bba69f9a3a0a00b89829d03590974dd
Eubiel178 <98427107+Eubiel178@users.noreply.github.com>
2022-09-17 20:40:25 -0300

    Delete src directory

--- src/components/Account/Body/Body.js
+++
@@ -1,7 +0,0 @@
-import { Container } from "./Styles";
-
-const Body = ({ children }) => {
-  return <Container>{children}</Container>;
-};
-
-export default Body;
--- src/components/Account/Header/Styles.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const HeaderContents = styled.div`
-  color: #50a59b;
-  text-align: center;
-  padding: 1.5em 0em;
-`;
