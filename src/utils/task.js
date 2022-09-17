574ec8ec1240f226fb4c8b8a214b7a585be56343
Eubiel178 <98427107+Eubiel178@users.noreply.github.com>
2022-09-17 08:34:20 -0300

    tarefas sendo salvas no localstorage sem bug

--- src/utils/task.js
+++ src/utils/task.js
@@ -1,3 +1,15 @@
-export const addTask = (tasks) => {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+export const addTask = (task) => {
+  if (localStorage.length > 0) {
+    let list = taskList();
+    list.push(task);
+    localStorage.setItem("tasks", JSON.stringify(list));
+  } else {
+    localStorage.setItem("tasks", JSON.stringify(task));
+  }
+};
+
+export const taskList = () => {
+  if (localStorage.length > 0) {
+    return JSON.parse(localStorage.getItem("tasks"));
+  }
 };
