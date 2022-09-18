32afd0731577577740e9177c171472df052fd06b
Eubiel178 <98427107+Eubiel178@users.noreply.github.com>
2022-09-18 11:44:44 -0300

    Add files via upload

---
+++ src/pages/TaskApp/components/SubTitle/Styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Feedback = styled.div`
+  h2 {
+    margin-top: 1.5em;
+    margin: 1.5em 0em 1em 0em;
+  }
+
+  p {
+    margin-left: 0.2em;
+    color: #686868;
+  }
+`;
