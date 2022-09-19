80ec3edb8dd0344036f74f2d9f1e95cffb1f95c9
Eubiel178 <98427107+Eubiel178@users.noreply.github.com>
2022-09-19 08:28:46 -0300

    Update Header.jsx

--- src/pages/TaskApp/components/Header/Header.jsx
+++ src/pages/TaskApp/components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import { HeaderContents } from "./Styles";
 
-import NavBar from "./NavBar/NavBar";
-
 const Header = () => {
   return (
     <header>
       <HeaderContents>
-        <NavBar />
+    
         <h1>ADICIONAR TAREFAS</h1>
       </HeaderContents>
     </header>
