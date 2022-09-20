1ea8dca7e834eb09115d3f50d5e615fc45bd6ba0
Eubiel178 <98427107+Eubiel178@users.noreply.github.com>
2022-09-20 10:25:39 -0300

    tentativa de corrigir bug

---
+++ src/utils/task.js
@@ -0,0 +1,45 @@
+export const taskList = () => {
+  if (localStorage.getItem("tasks")) {
+    let tasks = localStorage.getItem("tasks");
+    return JSON.parse(tasks);
+  }
+};
+
+export const removeTask = (taskid) => {
+  let list = taskList();
+
+  let newList = list.filter((task) => {
+    return taskid !== task.id;
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(newList));
+};
+
+export const addTask = (task) => {
+  let list = [];
+
+  if (localStorage.getItem("tasks")) {
+    list = taskList();
+    list.push(task);
+
+    localStorage.setItem("tasks", JSON.stringify(list));
+  } else {
+    list.push(task);
+
+    localStorage.setItem("tasks", JSON.stringify(list));
+  }
+};
+
+export const taskEdit = (task, id) => {
+  const list = taskList();
+
+  let mirror = [...list];
+
+  const target = mirror.findIndex((element) => {
+    return element.id === id;
+  });
+
+  mirror[target].description = task.description;
+
+  localStorage.setItem("tasks", JSON.stringify(mirror));
+};
