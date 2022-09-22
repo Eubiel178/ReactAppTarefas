374ee81b0595754c688eaaacc440978e0a1d2682
Eubiel178 <98427107+Eubiel178@users.noreply.github.com>
2022-09-22 19:10:16 -0300

    Adicionei mais feedbacks

--- src/pages/TaskApp/components/Header/Header.jsx
+++ src/pages/TaskApp/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import Swal from "sweetalert2";
 import Contexts from "../../../../contexts/Contexts";
 import { loggout } from "../../../../utils/user";
 import { HeaderContents } from "./Styles";
@@ -7,8 +8,26 @@ const Header = () => {
   const { setAuth } = useContext(Contexts);
 
   const handleLogin = () => {
-    loggout();
-    setAuth(false);
+    Swal.fire({
+      title: "Deseja deslogar?",
+
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sim!",
+      cancelButtonText: "Não",
+      preConfirm: (value) => {
+        if (value === true) {
+          loggout();
+          setAuth(false);
+        }
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Usuario deslogado com sucesso");
+      }
+    });
   };
 
   return (
