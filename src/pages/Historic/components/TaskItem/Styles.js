e2373da498aa82af9f3419f72d743e4d3c39d022
DevGabriel <dev123gabriel@gmail.com>
2022-10-04 14:23:52 -0300

    Alteracao no style

--- src/pages/Historic/components/TaskItem/Styles.js
+++ src/pages/Historic/components/TaskItem/Styles.js
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const Task = styled.li`
   text-align: justify;
   word-break: break-all;
+  padding-bottom: 1em;
   color: ${({ color }) => color};
 `;
