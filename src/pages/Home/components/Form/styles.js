a276132126350812f769a9bb43c9035f776d11b4
DevGabriel <dev123gabriel@gmail.com>
2022-10-14 19:32:48 -0300

    alteracao no style do form

--- src/pages/Home/components/Form/styles.js
+++ src/pages/Home/components/Form/styles.js
@@ -14,10 +14,11 @@ export const Button = styled.button`
   padding: 1em;
   color: #fff;
   background-color: ${({ backgroundButton }) => backgroundButton};
-  border-radius: 5px;
+  border-radius: 0.7em;
   border: 1px solid rgba(0, 0, 0, 0.137);
   font-size: 16px;
   transition-duration: 0.5s;
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.048);
 
   :hover {
     box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.192);
@@ -32,7 +33,6 @@ export const Button = styled.button`
 export const Input = styled.input`
   padding: 1em;
   width: 45%;
-  border-radius: 5px;
   font-size: 16px;
   border: 1px solid rgb(189, 183, 183);
   border-radius: 0.7em;
